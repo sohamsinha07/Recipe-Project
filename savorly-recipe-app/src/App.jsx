539f564d0f7d8e507aa73fbb1f6c11c761decceb
davidvillon04 <davidv04@vt.edu>
2025-06-02 14:02:02 -0400

    Allow navbar to appear on every page

--- savorly-recipe-app/src/App.jsx
+++ savorly-recipe-app/src/App.jsx
@@ -1,11 +1,11 @@
-import HomePage from "./routes/HomePage";
+import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <>
       <Navbar />
-      <HomePage />
+      <Outlet />
     </>
   );
 }
