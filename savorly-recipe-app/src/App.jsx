996aa02bdf17ffb37060bfc67ac605f15e045375
imjel <maxineendieveri@gmail.com>
2025-06-02 14:25:15 -0700

    app.jsx configed for details page debugging

--- savorly-recipe-app/src/App.jsx
+++ savorly-recipe-app/src/App.jsx
@@ -1,11 +1,13 @@
 import HomePage from "./routes/HomePage";
 import Navbar from "./components/Navbar";
+import RecipeDetailsPage from "./routes/RecipeDetailsPage";
 
 export default function App() {
   return (
     <>
-      <Navbar />
-      <HomePage />
+      {/* <Navbar />
+      <HomePage /> */}
+		<RecipeDetailsPage/>
     </>
   );
 }
