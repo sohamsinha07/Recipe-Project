c032d01b6d5a567267d6c39940adfbda8d4cc367
davidvillon04 <davidv04@vt.edu>
2025-06-02 12:00:29 -0400

    Created routes setup

---
+++ savorly-recipe-app/src/App.jsx
@@ -0,0 +1,11 @@
+import HomePage from "./routes/HomePage";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <HomePage />
+    </>
+  );
+}
---
+++ savorly-recipe-app/src/main.jsx
@@ -0,0 +1,25 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+
+import HomePage from "./routes/HomePage";
+import ProfilePage from "./routes/ProfilePage";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      // When someone visits "/"", load HomePage
+      { index: true, element: <HomePage /> },
+      { path: "profile", element: <ProfilePage /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
