f7dcea7aaaeb5585691e01fe6e03188f9a1cdd95
imjel <maxineendieveri@gmail.com>
2025-06-03 14:15:15 -0700

    quick fix app.jsx

--- savorly-recipe-app/src/App.jsx
+++ savorly-recipe-app/src/App.jsx
@@ -1,6 +1,5 @@
 import { useLocation, Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import { Routes, Route, Outlet} from "react-router-dom";
 
 
 export default function App() {
