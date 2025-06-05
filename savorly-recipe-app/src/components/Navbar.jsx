be44ae8f91c6f44e40a768a5c1a28e3d53823225
davidvillon04 <davidv04@vt.edu>
2025-06-05 17:22:52 -0400

    Update Navbar.jsx

--- savorly-recipe-app/src/components/Navbar.jsx
+++ savorly-recipe-app/src/components/Navbar.jsx
@@ -93,6 +93,14 @@ export default function Navbar() {
     },
   };
 
+  const handleNavClick = (path) => {
+    if (user) {
+      navigate(path);
+    } else {
+      setLoginOpen(true);
+    }
+  };
+
   return (
     <>
       <AppBar position="static" color="transparent">
@@ -125,8 +133,7 @@ export default function Navbar() {
             }}
           >
             <Button
-              component={NavLink}
-              to="/recipes"
+              onClick={() => handleNavClick("/recipes")}
               sx={{
                 color: (theme) => (theme.palette.mode === "light" ? "#777" : "#EEE"),
                 ...navLinkStyles,
@@ -145,8 +152,7 @@ export default function Navbar() {
               Community
             </Button> */}
             <Button
-              component={NavLink}
-              to="/my_kitchen"
+              onClick={() => handleNavClick("/my_kitchen")}
               sx={{
                 color: (theme) => (theme.palette.mode === "light" ? "#777" : "#EEE"),
                 ...navLinkStyles,
